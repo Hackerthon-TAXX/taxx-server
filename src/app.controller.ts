58105a1c700fe100172d1c6781b04ab51a271702
gwansikk <seorit98@gmail.com>
2023-06-28 22:10:18 +0900

    * NestJS 기본 세팅

---
+++ src/app.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get, UseInterceptors } from "@nestjs/common";
+import { AppService } from "./app.service";
+import { ApiTags } from "@nestjs/swagger";
+import { SuccessInterceptor } from "./common/interceptors/success.interceptor";
+
+@Controller()
+@UseInterceptors(SuccessInterceptor)
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,42 @@
+import { NestFactory } from "@nestjs/core";
+import { NestExpressApplication } from "@nestjs/platform-express";
+import { AppModule } from "./app.module";
+import * as expressBasicAuth from "express-basic-auth";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { ValidationPipe } from "@nestjs/common";
+import { HttpExceptionFilter } from "./common/exceptions/http-exception.filter";
+import helmet from "helmet";
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.use(helmet());
+  app.use(
+    ["/admin-docs", "/admin-docs-json"],
+    expressBasicAuth({
+      challenge: true,
+      users: {
+        [process.env.SWAGGER_USER]: process.env.SWAGGER_PASS,
+      },
+    })
+  );
+
+  const config = new DocumentBuilder().setTitle("").setVersion("1.0.0").setDescription("").build();
+
+  const swaggerCustomOptions = {
+    swaggerOptions: { docExpansion: "none" },
+  };
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("admin-docs", app, document, swaggerCustomOptions);
+
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
+
+  await app.listen(process.env.PORT || 3000);
+}
+bootstrap();
