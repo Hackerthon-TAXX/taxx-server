cf8bf1bb34924e0da98effe809433c4c1648740a
gwansikk <seorit98@gmail.com>
2023-06-29 12:11:30 +0900

    C: Users-Evals 관계 설정

--- src/evals/dto/evals.create.dto.ts
+++ src/evals/dto/evals.create.dto.ts
@@ -2,42 +2,39 @@ import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, IsNumber, IsObject, IsOptional, IsString } from "class-validator";
 
 export class EvlasCreateDto {
-    @ApiProperty({
-        description: "rider 고유 식별 번호",
-        example: 123,
-        required: true,
-    })
-    @IsNotEmpty()
-    @IsNumber()
-    // 외래키 설정 필요
-    rider: number;
-    
-    @ApiProperty({
-        description: "user 고유 식별 번호",
-        example: 123,
-        required: true,
-    })
-    @IsNotEmpty()
-    @IsNumber()
-    // 외래키 설정 필요
-    user: number;
-    
-    @ApiProperty({
-        description: "사용자가 평가한 평점",
-        example: 3.5,
-        required: true,
-    })
-    @IsNotEmpty()
-    @IsNumber()
-    rate: number;
+  @ApiProperty({
+    description: "사용 유저 UID",
+    example: 1000,
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsNumber()
+  usersId: number;
 
+  @ApiProperty({
+    description: "기사 ID",
+    example: 1,
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsNumber()
+  ridersId: number;
 
-    @ApiProperty({
-        description: "사용자가 남긴 평가",
-        example: "빠르고 좋아요~",
-        required: false,
-    })
-    @IsOptional()
-    @IsString()
-    comment: string;
+  @ApiProperty({
+    description: "사용자가 평가한 평점",
+    example: 3.5,
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsNumber()
+  rate: number;
+
+  @ApiProperty({
+    description: "사용자가 남긴 평가",
+    example: "빠르고 좋아요~",
+    required: false,
+  })
+  @IsOptional()
+  @IsString()
+  comment: string;
 }
