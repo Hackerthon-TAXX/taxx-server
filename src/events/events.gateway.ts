64c54b0b90c78a8ea83ac97de5bca25c4f678cc7
gwansikk <seorit98@gmail.com>
2023-06-29 22:55:10 +0900

    A: Websoket 기능 완성

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -1,35 +1,67 @@
+import { Logger } from "@nestjs/common";
 import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
 import { Server } from "ws";
 
-@WebSocketGateway(28080)
+@WebSocketGateway(10009)
 export class EventsGateway implements OnGatewayConnection, OnGatewayDisconnect {
-  client: Record<string, any>;
-  constructor() {
-    this.client = {};
-  }
-  @WebSocketServer()
-  server: Server;
+  private rooms: Record<string, any[]> = {};
+  @WebSocketServer() server: Server;
 
   public handleConnection(client): void {
-    const uid = String(Number(new Date()));
-    console.log("연결:", uid);
-    client["id"] = uid;
-    this.client[client["id"]] = client;
+    client["id"] = this.generateUniqueId();
+    // Logger.log(client["id"], "Connection");
   }
 
   public handleDisconnect(client): void {
-    console.log("해제:", client["id"]);
-    delete this.client[client["id"]];
+    Logger.debug(client["id"], "Disconnect");
+    this.removeClientFromRooms(client);
+  }
+
+  @SubscribeMessage("joinRoom")
+  joinRoom(client: any, room: string): void {
+    if (!this.rooms[room]) this.rooms[room] = [];
+    this.rooms[room].push(client);
+    Logger.log(`${room} - ${client.id}`);
+  }
+
+  @SubscribeMessage("leaveRoom")
+  leaveRoom(client: any, room: string): void {
+    const roomClients = this.rooms[room];
+
+    if (roomClients) {
+      this.removeClientFromRoom(client, room, roomClients);
+    }
   }
 
   @SubscribeMessage("events")
   handleMessage(client: any, payload: any): void {
-    for (const [key, value] of Object.entries(this.client)) {
-      value.send(
-        JSON.stringify({
-          id: client["id"],
-        })
-      );
+    for (const [room, clients] of Object.entries(this.rooms)) {
+      if (clients.includes(client)) {
+        this.sendPayloadToOtherClients(client, clients, payload);
+        break;
+      }
     }
+  }
+
+  private removeClientFromRooms(client): void {
+    for (const [room, clients] of Object.entries(this.rooms)) {
+      this.removeClientFromRoom(client, room, clients);
+    }
+  }
+
+  private removeClientFromRoom(client, room, clients): void {
+    const clientIndex = clients.indexOf(client);
+
+    if (clientIndex !== -1) {
+      clients.splice(clientIndex, 1);
+    }
+  }
+
+  private sendPayloadToOtherClients(client, clients, payload): void {
+    clients.filter((roomClient) => roomClient !== client).forEach((roomClient) => roomClient.send(JSON.stringify(payload)));
+  }
+
+  private generateUniqueId(): string {
+    return String(Number(new Date()));
   }
 }
